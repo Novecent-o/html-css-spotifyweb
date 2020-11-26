4d6cde1bac47e6dcb28de314d1561251117e599c
Novecento <ivan.monfrini@gmail.com>
2020-11-26 17:02:14 +0100

    Rivisto

--- js/script.js
+++ js/script.js
@@ -1,3 +1,20 @@
+$(document).ready(
+    function () {
+        
+        $('.green_menu').mouseenter(
+            function() {
+                $(this).addClass('active_green');
+            }
+        );
+        $('.green_menu').mouseleave(
+            function() {
+                $(this).removeClass('active_green');
+            }
+        );
+    }
+);
+
 document.getElementById('text-js').innerHTML = 'Artisti di strada senza speranza';
 document.getElementById('text-js2').innerHTML = 'Pensato per John Travolto';
 document.getElementById('text-js3').innerHTML = 'Ascolta queste playlist se ti vuoi far del male';
+
